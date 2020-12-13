a85353efa36f40de6bbd3b4419b97aa7e9cc3004
Xavier Morell Llorens <xmorelll@uoc.edu>
2020-12-13 18:49:45 +0100

    start styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,45 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    borderColor: theme => ({
+      ...theme('colors'),
+      'darkorange': '#ff8c00',
+      'lightorange': '#ffd199'
+    }),
+    backgroundColor: theme => ({
+      'darkblue': '#000078',
+      'lightpink': '#ececff',
+      'lightyellow': '#ffffec'
+
+    }),
+    extend: {
+      width: {
+        '22px': '22px',
+        '29px': '29px'
+      },
+      height: {
+        '22px': '22px',
+        '29px': '29px',
+        '32px': '32px'
+      }
+    },
+    screens: {
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '992px',
+      // => @media (min-width: 992px) { ... }
+
+      'xl': '1200px',
+      // => @media (min-width: 1200px) { ... }
+    }
   },
   variants: {
     extend: {},
   },
   plugins: [],
 }
+
